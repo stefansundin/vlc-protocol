3344a2c9798e76c2d1d334c0d95f767c344880fe
Stefan Sundin <git@stefansundin.com>
2016-12-16 09:28:23 -0800

    Add Mac README.md

--- mac/build.sh
+++ mac/build.sh
@@ -1,4 +1,3 @@
 #!/bin/bash -ex
 mkdir -p VLC-protocol-app/Contents/MacOS
 gcc -framework Cocoa -o VLC-protocol-app/Contents/MacOS/vlc-protocol vlc-protocol.m
-cp -r VLC-protocol-app/ /Applications/VLC-protocol.app
