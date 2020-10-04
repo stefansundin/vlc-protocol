9a7a8c50e3f4db1566da65afd3344ff16010d26f
Stefan Sundin <git@stefansundin.com>
2020-10-04 14:17:10 -0700

    Compile Windows ARM64 binary.

--- windows/exe/build.sh
+++ windows/exe/build.sh
@@ -1,2 +1,3 @@
 #!/bin/bash -ex
 x86_64-w64-mingw32-gcc -o vlc-protocol.exe vlc-protocol.c -mwindows -municode -lshlwapi -O2 -s
+aarch64-w64-mingw32-gcc -o vlc-protocol-arm64.exe vlc-protocol.c -mwindows -municode -lshlwapi -O2 -s
